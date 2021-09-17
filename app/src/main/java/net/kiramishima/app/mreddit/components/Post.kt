dc4871c887712ae0a6cc80c858c1e6973db9841f
Paul <kiramishima@outlook.com>
2021-09-16 22:03:07 -0500

    Agregando animaciones al JoinButton

--- app/src/main/java/net/kiramishima/app/mreddit/components/Post.kt
+++ app/src/main/java/net/kiramishima/app/mreddit/components/Post.kt
@@ -59,8 +59,8 @@ fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
 }
 
 @Composable
-fun Header(post: PostModel) {
-  Row(modifier = Modifier.padding(start = 16.dp)) {
+fun Header(post: PostModel, onJoinButtonClick: (Boolean) -> Unit = {}) {
+  Row(modifier = Modifier.padding(start = 16.dp), verticalAlignment = Alignment.CenterVertically) {
     Image(
       ImageBitmap.imageResource(id = R.drawable.subreddit_placeholder),
       contentDescription = stringResource(id = R.string.subreddits),
@@ -80,6 +80,8 @@ fun Header(post: PostModel) {
         color = Color.Gray
       )
     }
+    Spacer(modifier = Modifier.width(4.dp))
+    JoinButton(onJoinButtonClick)
     MoreActionsMenu()
   }
   
---
+++ app/src/main/java/net/kiramishima/app/mreddit/screens/ChooseCommunityScreen.kt
@@ -0,0 +1,130 @@
+package net.kiramishima.app.mreddit.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import net.kiramishima.app.mreddit.R
+import net.kiramishima.app.mreddit.routing.MRedditRouter
+import net.kiramishima.app.mreddit.viewmodel.MainViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import net.kiramishima.app.mreddit.routing.BackButtonAction
+
+private const val SEARCH_DELAY_MILLIS = 300L
+
+private val defaultCommunities = listOf("programming4food", "androiddev", "puppies")
+
+@Composable
+fun ChooseCommunityScreen(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+  val scope = rememberCoroutineScope()
+  val communities: List<String> by viewModel.subreddits.observeAsState(emptyList())
+  var searchedText by remember { mutableStateOf("") }
+  var currentJob by remember { mutableStateOf<Job?>(null) }
+  val activeColor = MaterialTheme.colors.onSurface
+
+  LaunchedEffect(Unit) {
+    viewModel.searchCommunities(searchedText)
+  }
+
+  Column {
+    ChooseCommunityTopBar()
+    TextField(
+      value = searchedText,
+      onValueChange = {
+        searchedText = it
+        currentJob?.cancel()
+        currentJob = scope.async {
+          delay(SEARCH_DELAY_MILLIS)
+          viewModel.searchCommunities(searchedText)
+        }
+      },
+      leadingIcon = {
+        Icon(Icons.Default.Search, contentDescription = stringResource(id = R.string.search))
+      },
+      label = { Text(stringResource(id = R.string.search)) },
+      modifier = modifier
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp),
+      colors = TextFieldDefaults.outlinedTextFieldColors(
+        focusedBorderColor = activeColor,
+        focusedLabelColor = activeColor,
+        cursorColor = activeColor,
+        backgroundColor = MaterialTheme.colors.surface
+      )
+    )
+    SearchedCommunities(communities, viewModel, modifier)
+    BackButtonAction {
+      MRedditRouter.goBack()
+    }
+  }
+}
+
+@Composable
+fun SearchedCommunities(
+  communities: List<String>,
+  viewModel: MainViewModel?,
+  modifier: Modifier = Modifier
+) {
+  communities.forEach {
+    Community(
+      text = it,
+      modifier = modifier,
+      onCommunityClicked = {
+        viewModel?.selectedCommunity?.postValue(it)
+        MRedditRouter.goBack()
+      }
+    )
+  }
+}
+
+@Composable
+fun ChooseCommunityTopBar(modifier: Modifier = Modifier) {
+
+  val colors = MaterialTheme.colors
+
+  TopAppBar(
+    title = {
+      Text(
+        fontSize = 16.sp,
+        text = stringResource(R.string.choose_community),
+        color = colors.primaryVariant
+      )
+    },
+    navigationIcon = {
+      IconButton(
+        onClick = { MRedditRouter.goBack() }
+      ) {
+        Icon(
+          imageVector = Icons.Default.Close,
+          tint = colors.primaryVariant,
+          contentDescription = stringResource(id = R.string.close)
+        )
+      }
+    },
+    backgroundColor = colors.primary,
+    elevation = 0.dp,
+    modifier = modifier
+      .height(48.dp)
+      .background(Color.Blue)
+  )
+}
+
+@Preview
+@Composable
+fun SearchedCommunitiesPreview() {
+  Column {
+    SearchedCommunities(defaultCommunities, null, Modifier)
+  }
+}
